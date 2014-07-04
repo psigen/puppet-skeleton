d07524510e47b80d6f899103506c21cb94270e05
Pras Velagapudi <psigen@gmail.com>
2014-07-04 14:36:54 -0400

    Added some dep handling to update script.

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'librarian-puppet', '~> 1.1.x'
+
+group :test do
+  gem 'puppet-lint'
+end
--- update.sh
+++ update.sh
@@ -7,13 +7,72 @@
 #
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
-echo "Starting puppet update..."
+# Default paths to various tools.
+APT=/usr/bin/apt-get
+GEM=/usr/bin/gem
+BUNDLE=/usr/bin/bundle
+PUPPET=/usr/bin/puppet
+LIBRARIAN=/usr/bin/librarian-puppet
+
+# Change to this script's directory.
 pushd ${DIR} > /dev/null
 
-sudo puppet apply --hiera_config ./hiera.yaml \
-                  --modulepath ./modules \
-                  manifests/site.pp \
+# Warn the user if not running as root. (This may be desired in some special
+# cases, but most commands probably won't work)
+if [[ $EUID -ne 0 ]]; then
+   echo "WARNING: Script was not run as root, install commands may fail!" >&2
+fi
+
+# Install puppet if it is not already installed.
+if [ ! -x ${PUPPET} ]; then
+	echo "Puppet was not found at '${PUPPET}'."
+	echo "Installing Puppet from apt..."
+
+	${APT} install -y puppet-common
+
+	echo "Puppet installation complete."
+fi
+
+# Install librarian-puppet if it is not already installed.
+if [ ! -x ${LIBRARIAN} ]; then
+	echo "Librarian-puppet was not found at '${LIBRARIAN}'."
+	echo "Installing librarian-puppet from gem..."
+
+    if [ ! -x ${BUNDLE} ]; then
+		if [ ! -x ${GEM} ]; then
+			# Install gem if it is not already installed.
+			${APT} install -y rubygems
+		fi
+	
+		# Install bundle if it is not already installed.
+		${GEM} install bundler
+	fi
+
+	# Install librarian-puppet via the Gemfile.
+	${BUNDLE} install
+
+	echo "Librarian-puppet installation complete."
+fi
+
+# TODO: remove this
+echo "DEBUG EXIT"
+exit 0
+
+# Initialize librarian-puppet if necessary.
+if [ ! -d .librarian ]; then
+	echo "Initializing librarian-puppet from Puppetfile..."
+
+	${LIBRARIAN} install
+
+	echo "Librarian-puppet initialized."
+fi
+
+# Apply the puppet manifest from this repository.
+echo "Starting puppet update..."
+
+${PUPPET} apply --hiera_config ./hiera.yaml \
+                --modulepath ./modules \
+                manifests/site.pp \
     || { echo "ERROR: Failure during puppet configuration." && exit 1; }
 
-popd > /dev/null
 echo "Update complete."
